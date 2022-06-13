1edeee74b837f1adea4035f1e2fa1ca5fc2fe8e3
D1ck7 <dkorovin155@gmail.com>
2022-06-13 20:35:28 +0300

    added benefits section, added top section, fixed header, fixed slider section

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,8 @@ $('.slider__inner').slick({
   fade:true
 });
 
+var mixer = mixitup('.top__content');
+
+
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const imagemin      = require('gulp-imagemin');
 const del           = require('del');
 const browserSync   = require('browser-sync').create();
 
+
 function browsersync() {
   browserSync.init({
     server: {
@@ -33,6 +34,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
