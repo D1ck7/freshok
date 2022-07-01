99e68558dd7884ffd7577fa6e78d6b3fb163aa08
D1ck7 <dkorovin155@gmail.com>
2022-07-01 19:00:18 +0300

    fixed header, top__slider, benefits, top-products

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,21 @@
 $(function(){
 
-$('.slider__inner').slick({
-  fade:true
+$('.top-slider__inner').slick({
+  fade:true,
+  prevArrow: $('.top-slider__btn--prev'),
+  nextArrow: $('.top-slider__btn--next')
 });
 
-var mixer = mixitup('.top__content');
+$(".rating__star").rateYo({
+    numStars: 1,
+    fullStar: true,
+    starWidth: "16px",
+    
+  });
+
+var mixer = mixitup('.top-products__content');
+
+var mixer = mixitup('.promotions__content');
 
 
 
